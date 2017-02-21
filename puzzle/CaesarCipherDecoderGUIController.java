d2d8911ba3ac6a3f7f4a053dae25c69e46eac75d
Drew Hans <drewhans555@gmail.com>
2017-02-21 10:43:36 -0600

    DrewHans - added a puzzle package

---
+++ puzzle/CaesarCipherDecoderGUIController.java
@@ -0,0 +1,22 @@
+package puzzle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Drew.Hans
+ */
+public class CaesarCipherDecoderGUIController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
