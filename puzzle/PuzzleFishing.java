06b6b60749fbbab314f1b8e07f94b9f7a5ec8608
Jessica <Jessica@10.150.4.172>
2017-03-06 22:46:01 -0600

    Working on two puzzles

--- puzzle/PuzzleFishing.java
+++ puzzle/PuzzleFishing.java
@@ -2,13 +2,14 @@ package puzzle;
 
 public class PuzzleFishing extends PuzzleModel {
     
- 
-
-    /**
-     * Superclass Constructor
-     */
+    private final String waterLocation = "water.jpg";
+    int smallFishWeight = 1;
+    int mediumFishWeight = 3;
+    int largeFishWeight = 5;
+    
+    
     public PuzzleFishing() {
-        this.setBackground("location");
+        this.setBackground(waterLocation);
         this.setSound("");
         this.setText("I row out to my normal fishing spot. I’ll need 5 fish to "
                 + "sell at the market today.”\n Instructions: Click on the "
