64a1bcada88a4e9ae2f762bc1c6d0fbf09d5d77d
lucas.burdell <lucas.burdell@blackburn.edu>
2017-03-07 09:56:04 -0600

    add fishing puzzle controller

---
+++ puzzle/PuzzleFishingController.java
@@ -0,0 +1,39 @@
+package puzzle;
+
+import gui.ChallengeController;
+import gui.ChallengeStatus;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author lucas.burdell
+ */
+public class PuzzleFishingController extends ChallengeController
+        implements Initializable {
+
+    @FXML
+    private Pane gamePane;
+    @FXML
+    private ImageView fish;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void clickedFish(MouseEvent event) {
+        this.finishChallenge(ChallengeStatus.WIN);
+    }
+    
+}
