eccc6783fab38d103d53adf1482817536c40dfc2
Jessica <Jessica@10.150.4.172>
2017-03-07 22:48:05 -0600

    Working on two puzzles

--- puzzle/PuzzleCaesarCipher.java
+++ puzzle/PuzzleCaesarCipher.java
@@ -1,6 +1,40 @@
 package puzzle;
 
-public class PuzzleCaesarCipher {
+public class PuzzleCaesarCipher extends PuzzleModel {
     
+    private final String answer = "";
+    private final String originalMessage = "";
     
+    public PuzzleCaesarCipher(){
+        this.setBackground("");
+        this.setSound("");
+        this.setText("I examine the box more closely and I notice a jumble "
+                + "of letters carved into the top of the box that reads ... " +
+                this.originalMessage + ". A closer examination reveals"
+                + "something else on the bottom: A = C, B = D ...  It appears "
+                + "that I have to translate the message and enter it into the "
+                + "lock.");
+    }
+
+    /**
+     * @return the answer String
+     */
+    public String getAnswer() {
+        return this.answer;
+    }
+
+    /**
+     * guess is called by the controller when the player makes a guess and
+     * decides which text to set for the controller
+     *
+     * @param g
+     */
+    public void guess(String g) {
+        if (this.answer.equalsIgnoreCase(g)) {
+            this.setText("It seems to have worked!");
+            //opening sound
+        } else {
+            this.setText("Nothing happened. Maybe I should try again.");
+        }
+    }
 }
--- puzzle/PuzzleFishing.java
+++ puzzle/PuzzleFishing.java
@@ -1,20 +1,86 @@
 package puzzle;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.scene.control.Button;
+
 public class PuzzleFishing extends PuzzleModel {
     
-    private final String waterLocation = "water.jpg";
-    int smallFishWeight = 1;
-    int mediumFishWeight = 3;
-    int largeFishWeight = 5;
+    private final String waterPic = "water.jpg";
+    private final String smallFishPic = "";
+    private final String mediumFishPic = "";
+    private final String largeFishPic = "";
+    private final String newFishSound = "";
+    private int fishCount = 0;
+    private final Button smallFishButton = null;
+    private final Button mediumFishButton = null;
+    private final Button largeFishButton = null;
+    private int fishWeight = 0; 
     
-    
-    public PuzzleFishing() {
-        this.setBackground(waterLocation);
-        this.setSound("");
+    public PuzzleFishing() throws InterruptedException {
+        this.setBackground(waterPic);
+        this.setSound(newFishSound);
         this.setText("I row out to my normal fishing spot. I’ll need 5 fish to "
                 + "sell at the market today.”\n Instructions: Click on the "
                 + "fish to catch them. Bigger fish will disappear faster, "
                 + "but are worth more.");
+        
+        while(fishCount <= 5){
+            this.displayFish();
+        }
+    }
+    
+    public int getRandomFishSize(){
+        Random rand = null;
+        int randomFish = rand.nextInt((3-1)+1);
+        return randomFish;
     }
 
+    public void displayFish() throws InterruptedException{
+        int fish = this.getRandomFishSize();
+        this.setSound(this.newFishSound);
+        TimerTask smallFishTask = new TimerTask() {
+            @Override
+            public void run() {
+                //display small fish
+            }
+        };
+        TimerTask mediumFishTask = new TimerTask() {
+            @Override
+            public void run() {
+                //Display medium fish
+            }
+        };
+        TimerTask largeFishTask = new TimerTask() {
+            @Override
+            public void run() {
+                //Display Large fish
+            }
+        };
+        Timer timer = new Timer();
+        if (fish ==1){
+            timer.schedule(smallFishTask, 5000);
+            //if(fish is clicked){
+            //    timer.cancel();
+            //    fishCount++;
+            //    weightCount += 1;
+            //}
+        } else if(fish == 3){
+            timer.schedule(mediumFishTask, 3000);
+            //if(fish is clicked){
+            //    timer.cancel();
+            //    fishCount++;
+            //    weightCount += 2;
+            //}
+        } else {
+            timer.schedule(largeFishTask, 1000);
+            //if(fish is clicked){
+            //    timer.cancel();
+            //    fishCount++;
+            //    weightCount += 3;
+            //}
+        }
+    }
+    
 }
