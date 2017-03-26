ab41d1ecc33f7a17c9a699983458168947edff2e
timothy.francis <timothy.francis@HU130C05.Blackburn.College>
2017-03-26 16:52:45 -0500

    created schema, untested. started working on loadstory.sql

--- sql/loadSTORY.sql
+++ sql/loadSTORY.sql
@@ -4,3 +4,8 @@
  */
 
 INSERT INTO TEST (TNAME, TTEXT, TVALUE) VALUES ('Lucas', 'my name is Lucas', 600);
+
+insert into actions(text) values ('I greet my wife. I am a fully operational human being, and definitely not a robot.');
+insert into actions(text) values ('I greet my wife. I am a fully operational human being, and definitely not a robot.');
+
+insert into conditional(objectid, flag, flagstate, actionid) values ()
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,21 +1,45 @@
-/**
- * Author:  lucas.burdell
- * Created: Mar 21, 2017
- */
+CREATE TABLE event (
+    eventID INTEGER NOT NULL Primary Key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    eventname varchar(64) not null,
+    text varchar(64) not null,
+    backgroundname varchar(64),
+    music varchar(64)
+);
 
-DROP TABLE TEST;
+CREATE TABLE choice (
+    choiceid integer not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    choicename varchar(64) not null,
+    eventid integer not null,
+    text varchar(64) not null,
+    actionid integer not null
+);
 
-CREATE TABLE TEST
-    (
-        TNAME VARCHAR(64) NOT NULL,
-        TTEXT VARCHAR(200),
-        TVALUE INTEGER NOT NULL
+CREATE TABLE actions (
+    actionid integer not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    text varchar(64)
+);
+
+CREATE TABLE conditional(
+    conditionalid integer not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    objectid integer not null,
+    flag varchar(64) not null,
+    flagstate integer not null,
+    actionid integer not null
+);
+
+CREATE TABLE challenge(
+    challengeid integer not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    challengename varchar(64) not null,
+    challengetype varchar(64) not null
+);
+
+CREATE TABLE actionsevent(
+    actionid integer not null,
+    eventid integer not null
+);
+
+ALTER TABLE actionsevent
+    add constraint ae_pk primary key (
+        actionid,
+        eventid
     );
-
-
-ALTER TABLE TEST
-    ADD CONSTRAINT TEST_PK Primary Key (
-            TNAME,
-            TVALUE
-        );
-
