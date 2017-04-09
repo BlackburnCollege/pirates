496456dcc0cc08df28fb3ac643d0b236faebd50c
Drew Hans <drewhans555@gmail.com>
2017-04-09 16:04:58 -0500

    DrewHans - updated PuzzleCaesarCipher code

--- puzzle/PuzzleCaesarCipher.java
+++ puzzle/PuzzleCaesarCipher.java
@@ -16,8 +16,8 @@ public class PuzzleCaesarCipher extends PuzzleModel {
     private int currentKey;
 
     // PuzzleCaesarCipher resource locations
-    private final String decoderBackgroundLocation = "decoder_base_0";
-    private final String decoderLocation = "decoder_0";
+    private final String decoderOuterLocation = "caesarcipherdecoderOUTER";
+    private final String decoderInnerLocation = "caesarcipherdecoderINNER";
     private final Sound soundClickLocation = Sound.CLICK;
 
     /**
@@ -29,17 +29,17 @@ public class PuzzleCaesarCipher extends PuzzleModel {
         this.currentKey = 0; // store currentKey
 
         // update model parameters for controller
-        this.setBackground(this.decoderBackgroundLocation);
+        this.setBackground(this.decoderOuterLocation);
 
         //TODO: SET SOUND TO LEVERPULL
         this.setSound(this.soundClickLocation);
-        this.setText("The decode is set to " + this.currentKey + ".");
+        this.setText("Ymj pnqqjw nx Qzhfx");
     }
 
     /**
      * Constructor
      *
-     * @param solutionKey
+     * @param solutionKey - the decoder key solution
      */
     public PuzzleCaesarCipher(int solutionKey) {
         this.correctKey = solutionKey; // store default correctKey
@@ -47,11 +47,11 @@ public class PuzzleCaesarCipher extends PuzzleModel {
         this.currentKey = 0; // store currentKey
 
         // update model parameters for controller
-        this.setBackground(this.decoderBackgroundLocation);
+        this.setBackground(this.decoderOuterLocation);
 
         //TODO: SET SOUND TO LEVERPULL
         this.setSound(this.soundClickLocation);
-        this.setText("The decode is set to " + this.currentKey + ".");
+        this.setText("");
     }
 
     /**
@@ -71,15 +71,52 @@ public class PuzzleCaesarCipher extends PuzzleModel {
     /**
      * @return the String location of the decoder background
      */
-    public String getDecoderBackgroundLocation() {
-        return this.decoderBackgroundLocation;
+    public String getDecoderOuterLocation() {
+        return this.decoderOuterLocation;
     }
 
     /**
      * @return the String location of the decoder
      */
-    public String getDecoderLocation() {
-        return this.decoderLocation;
+    public String getDecoderInnerLocation() {
+        return this.decoderInnerLocation;
+    }
+
+    /**
+     * private modifier restricts other programs, subclasses in this package,
+     * and different packages from accessing this method directly
+     *
+     * onTurn looks at the class variables values and decides which sound to set
+     * for the controller
+     */
+    private void onTurn() {
+        this.setSound(this.soundClickLocation);
+    }
+
+    /**
+     * turnClockwise is called by the controller when the player turns the
+     * decoder clockwise
+     */
+    public void turnClockwise() {
+        if (this.currentKey == 0) {
+            this.currentKey = 25;
+        } else {
+            this.currentKey--;
+        }
+        this.onTurn();
+    }
+
+    /**
+     * turnCounterClockwise is called by the controller when the player turns
+     * the decoder counterclockwise
+     */
+    public void turnCounterClockwise() {
+        if (this.currentKey == 25) {
+            this.currentKey = 0;
+        } else {
+            this.currentKey++;
+        }
+        this.onTurn();
     }
 
 }
--- puzzle/PuzzleCaesarCipherController.java
+++ puzzle/PuzzleCaesarCipherController.java
@@ -1,9 +1,146 @@
 package puzzle;
 
+import gui.AudioController;
+import gui.ChallengeController;
+import gui.ChallengeStatus;
+import gui.ImageController;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.MediaPlayer;
+
 /**
  *
  * @author Drew Hans
  */
-public class PuzzleCaesarCipherController {
+public class PuzzleCaesarCipherController extends ChallengeController implements Initializable {
+
+    @FXML
+    private Pane gamePane;
+
+    @FXML
+    private ImageView decoderBackground; // the outer decoder ring
+
+    @FXML
+    private ImageView decoder; // the inner decoder ring
+
+    @FXML
+    private Hyperlink leaveButton; // the exit button
+
+    @FXML
+    private MediaPlayer mediaPlayer; // plays sounds
     
+    @FXML
+    private TextField decoderKey; // the current decoder key
+
+    private PuzzleCaesarCipher puzzle;  // the PuzzleObject
+
+    private final ImageController images = ImageController.get();
+
+    /**
+     * initialize method
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.puzzle = new PuzzleCaesarCipher(5);
+
+        this.decoderBackground.fitWidthProperty().bind(gamePane.prefWidthProperty());
+        this.decoderBackground.fitHeightProperty().bind(gamePane.prefHeightProperty());
+        this.decoderBackground.setImage(images.getImage(this.puzzle.getDecoderOuterLocation()));
+
+        this.decoder.fitWidthProperty().bind(gamePane.prefWidthProperty());
+        this.decoder.fitHeightProperty().bind(gamePane.prefHeightProperty());
+        this.decoder.setImage(images.getImage(this.puzzle.getDecoderInnerLocation()));
+        
+        this.decoderKey.setText("5");
+
+        this.gamePane.requestFocus();
+
+        this.leaveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                PuzzleCaesarCipherController.this.finishChallenge(ChallengeStatus.LOSS);
+            }
+        });
+    }
+
+    /**
+     * checkSolution method interacts with ChallengeController superclass
+     * through finishChallenge method
+     */
+    private void checkSolution() {
+        System.out.println(puzzle.getCurrentKey());
+        if (puzzle.getCompleted() == true) {
+            this.finishChallenge(ChallengeStatus.WIN);
+        }
+    }
+
+    /**
+     * onKeyEvent method handles key presses
+     */
+    @FXML
+    private void onKeyEvent(KeyEvent event) {
+        switch (event.getCode()) {
+            case LEFT:
+            case A:
+            case KP_LEFT:
+                puzzle.turnCounterClockwise();
+                break;
+            case RIGHT:
+            case D:
+            case KP_RIGHT:
+                puzzle.turnClockwise();
+                break;
+            default:
+                break;
+        }
+
+        // update the GUI - rotate the inner decoder image
+        this.decoderBackground.setRotate((puzzle.getCurrentKey() / 26.0) * -360.0);
+
+        // play the appropriate sound (determined by the PuzzleObject)
+        AudioController.get().playSound(this.puzzle.getSound());
+        // check solve status
+        checkSolution();
+    }
+
+    /**
+     * onMouseEvent method handles mouse clicks and movement
+     */
+    @FXML
+    private void onMouseEvent(MouseEvent event) {
+    }
+
+    private EventHandler keyListener = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            onKeyEvent(event);
+        }
+
+    };
+
+    @Override
+    public void onChallengeLoaded() {
+
+    }
+
+    @Override
+    public void setupListeners(Scene scene) {
+        scene.setOnKeyPressed(keyListener);
+    }
+
+    @Override
+    public void teardownListeners(Scene scene) {
+        scene.onKeyPressedProperty().set(null);
+    }
 }
