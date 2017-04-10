471e1b1ec634cbcd77010b0080ef68313824d692
dakota.tebbe <dakota.tebbe@HU130C03.Blackburn.College>
2017-04-10 12:39:16 -0500

    did things

--- sql/combatDB/load_combat.sql
+++ sql/combatDB/load_combat.sql
@@ -9,8 +9,17 @@ INSERT INTO entitydata (ENTITY_NAME, HEALTH, MELEE_MODIFIER,
 RANGED_MODIFIER, VERBAL_MODIFIER, INSULT_IMMUNITY) VALUES (
     'wolf', 
     100, 
-    1.0, 
+    6.25, 
     0.0, 
     0.0,
     true
+);
+INSERT INTO entitydata (ENTITY_NAME, HEALTH, MELEE_MODIFIER, 
+RANGED_MODIFIER, VERBAL_MODIFIER, INSULT_IMMUNITY) VALUES (
+    'Demo Drew', 
+    100, 
+    2.5, 
+    6.25, 
+    0.0,
+    false
 );
