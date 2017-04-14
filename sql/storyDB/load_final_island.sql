59b9902a9abcd575a42da93e093c2f233d42ec49
DrewHans555 <drewhans555@live.com>
2017-04-14 09:38:37 -0500

    DrewHans - wrote some comments

--- sql/storyDB/load_final_island.sql
+++ sql/storyDB/load_final_island.sql
@@ -10,3 +10,8 @@
 # INSERT INTO choice(id, eventid, text, actionid) VALUES (1101, 1100, 'next', 1102);
 # INSERT INTO actions(id) VALUES (1102);
 # INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1102, 1103, 0);
+#
+# INSERT INTO event(id, text, backgroundname) values(1103,'TEXTGOESHERE', '');
+# INSERT INTO choice(id, eventid, text, actionid) values (1104, 1103, 'TEXTGOESHERE', 6);
+# INSERT INTO actions(id) values (1105);
+# INSERT INTO actionsevent (actionid, eventid, eventposition) values (1105, 1106, 0);
