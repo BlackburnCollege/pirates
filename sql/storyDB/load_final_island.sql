004a5e6a9ace12b0db6b509ff3c991b5fb175944
Drew Hans <drewhans555@gmail.com>
2017-04-14 09:30:23 -0500

    DrewHans - added comments to load_final_island.sql

--- sql/storyDB/load_final_island.sql
+++ sql/storyDB/load_final_island.sql
@@ -1,3 +1,12 @@
-/*
- FINAL ISLAND STARTS AT ID 1100
-*/
+/**
+ * FINAL ISLAND STARTS AT ID 1100
+ *
+ * @author Drew Hans
+ * @created: April 14, 2017
+ */
+
+# INSERT INTO aceobject(id, acetype) VALUES (1, 'event');
+# INSERT INTO event(id, text, backgroundname, music) VALUES (1100, 'TEXTGOESHERE', 'cave', 'LIVING_VOYAGE');
+# INSERT INTO choice(id, eventid, text, actionid) VALUES (1101, 1100, 'next', 1102);
+# INSERT INTO actions(id) VALUES (1102);
+# INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1102, 1103, 0);
