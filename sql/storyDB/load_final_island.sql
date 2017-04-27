5b02d55f5bc2987c932ebf458a65d94827afe725
Drew Hans <drewhans555@gmail.com>
2017-04-27 08:43:54 -0500

    DrewHans - created load_save_family.sql

--- sql/storyDB/load_final_island.sql
+++ sql/storyDB/load_final_island.sql
@@ -619,24 +619,27 @@ INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1309, 1310, 
      INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1333, 1334, 0);  -- jump to 1334
 
      INSERT INTO event(id, text) VALUES (1334, 'GAME OVER - You reached the bad end.');
---     INSERT INTO choice(id, eventid, text, actionid) VALUES (1335, 1334, 'next', 1336);
---     INSERT INTO actions(id) VALUES (1336);
---     INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1336, 1337, 0);  -- jump to 1337
---
+     INSERT INTO choice(id, eventid, text, actionid) VALUES (1335, 1334, 'See credits screen.', 1336);
+     INSERT INTO actions(id, challengeid) VALUES (1336, 1337);
+     INSERT INTO challenge(challengeid, challengename, challengetype) VALUES (1337, 'credits', 'other');
 ----
 -- Choice 2 - Save your family.
    INSERT INTO choice(id, eventid, text, actionid) VALUES (1338, 1310, 'Save your family.  No amount of gold is worth their lives.', 1339);
    INSERT INTO actions(id, text) VALUES (1339, 'You decide to save your family.');
    INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1339, 1340, 0);  -- jump to 1340
 ---- Choice 2 Results - Lead to first-mate confrontation.
-     INSERT INTO event(id, text) VALUES (1340, 'You take a piece of gold from the room as proof that you found the treasure, shut and scramble the lock, and then prepare to return to your docked ship.');
---     INSERT INTO choice(id, eventid, text, actionid) VALUES (1341, 1340, 'next', 1342);
---     INSERT INTO actions(id) VALUES (1342);
---     INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1342, 1343, 0);  -- jump to 1343
---
+     INSERT INTO event(id, text) VALUES (1340, 'You take a piece of gold from the room as proof that you found the treasure, shut and scramble the lock, and then begin making your way out of the cave.');
+     INSERT INTO choice(id, eventid, text, actionid) VALUES (1341, 1340, 'next', 1342);
+     INSERT INTO actions(id) VALUES (1342);
+     INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1342, 1343, 0);  -- jump to 1343
+
+     INSERT INTO event(id, text) VALUES (1343, 'You exit the cave and start heading down the path when you hear someone laugh.');
+     INSERT INTO choice(id, eventid, text, actionid) VALUES (1344, 1343, 'next', 1345);
+     INSERT INTO actions(id) VALUES (1345);
+     INSERT INTO actionsevent (actionid, eventid, eventposition) VALUES (1345, 1346, 0);  -- jump to 1346
+----
 ------------------------------------------------------------------------------------------------------------------------
--- Note:  1337 should lead to GAME OVER screen and then the game should end
--- Note:  1343 shoudl lead to the final confrontation scene
+-- Note:  1346 should lead to the final confrontation scene
 
 -- challenge for credits:
 -- INSERT INTO challenge(challengeid, challengename, challengetype) VALUES (1338, 'credits', 'other');
